741745377630bc57a45da3d2a015e6658e4e3188
Louis Aussedat <aussedat.louis@gmail.com>
2023-01-15 22:13:09 +0100

    component/AppBar: add Avatar showing connected relays

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -1,21 +1,42 @@
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
-import * as React from "react";
-import { Appbar as AppBarPaper } from "react-native-paper";
+import { useNostr } from "nostr-react";
+import { View } from "react-native";
+import { Appbar as AppBarPaper, Avatar, useTheme } from "react-native-paper";
+import { DEFAULT_RELAYS_URL } from "../../constant/relay";
 
-const AppBar = ({ navigation, back }: NativeStackHeaderProps) => (
-  <AppBarPaper.Header>
-    {back ? <AppBarPaper.BackAction onPress={navigation.goBack} /> : null}
-    <AppBarPaper.Content title="Anostr" />
-    <AppBarPaper.Action icon="magnify" onPress={() => {}} />
-    <AppBarPaper.Action
-      icon="account-circle-outline"
-      onPress={() => navigation.navigate("Profile")}
-    />
-    <AppBarPaper.Action
-      icon="cog"
-      onPress={() => navigation.navigate("Settings")}
-    />
-  </AppBarPaper.Header>
-);
+const AppBar = ({ navigation, back }: NativeStackHeaderProps) => {
+  const { connectedRelays } = useNostr();
+  const theme = useTheme();
+
+  return (
+    <AppBarPaper.Header>
+      {back ? <AppBarPaper.BackAction onPress={navigation.goBack} /> : null}
+      <AppBarPaper.Content title="Anostr" />
+      <AppBarPaper.Action icon="magnify" onPress={() => {}} />
+      <AppBarPaper.Action
+        icon="account-circle-outline"
+        onPress={() => navigation.navigate("Profile")}
+      />
+      <AppBarPaper.Action
+        icon="cog"
+        onPress={() => navigation.navigate("Settings")}
+      />
+      <View>
+        <Avatar.Text
+          size={24}
+          label={connectedRelays.length.toString()}
+          style={{
+            backgroundColor:
+              connectedRelays.length === 0
+                ? theme.colors.error
+                : connectedRelays.length === DEFAULT_RELAYS_URL.length
+                ? theme.colors.secondary
+                : theme.colors.error,
+          }}
+        />
+      </View>
+    </AppBarPaper.Header>
+  );
+};
 
 export default AppBar;
