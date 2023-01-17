9a01b4237c40935b254cc3666555cfb8c5278ca5
Louis Aussedat <louis.aussedat@equans.com>
2023-01-17 14:08:23 +0100

    screens/Home: remove unused store and imports

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,15 +4,13 @@ import { dateToUnix, useNostrEvents } from "nostr-react";
 import { Event } from "nostr-tools";
 import { useEffect, useState } from "react";
 import { ScrollView, StyleSheet } from "react-native";
-import { FAB, Text } from "react-native-paper";
+import { FAB } from "react-native-paper";
 import Post from "../components/Post";
 import { RootStackParamList } from "../navigation";
-import { useStores } from "../store";
 
 type HomeScreenProps = NativeStackScreenProps<RootStackParamList, "Home">;
 
 const HomeScreen = ({ route, navigation }: HomeScreenProps) => {
-  const { userStore } = useStores();
   const { events, unsubscribe } = useNostrEvents({
     filter: {
       kinds: [1],
