557f685c342d03c5d4a7acfcb1c34e11f9349b19
Louis Aussedat <louis.aussedat@equans.com>
2023-01-30 17:25:53 +0100

    eslint: add eslint recommended

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
