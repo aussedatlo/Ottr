37115d66ae703b4dc0bd7c67b05a28cec6e08b1f
Louis Aussedat <aussedat.louis@gmail.com>
2023-04-07 21:49:17 +0200

    screens/settings: improve settings view

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,6 +1,7 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Divider, Text } from 'react-native-paper';
 import ModalController from '../../components/Modal/ModalController';
 import { SettingsStackParamList } from '../../navigation/SettingsNavigation';
 import ButtonSettings from './ButtonSettings';
@@ -15,40 +16,63 @@ type SelectContactScreenProps = NativeStackScreenProps<
 const SettingsScreen = ({ navigation }: SelectContactScreenProps) => {
   return (
     <View>
+      <Text variant="labelMedium" style={styles.title}>
+        Account settings
+      </Text>
       <ButtonSettings
-        icon="account-circle-outline"
+        icon="account-edit"
         title="Profile"
         onPress={() => navigation.navigate('Profile')}
       />
+      <Divider />
       <ButtonSettings
-        icon="theme-light-dark"
-        title="Theme"
-        onPress={() => ModalController.showModal('Theme', <SelectThemeModal />)}
-      />
-      <ButtonSettings
-        icon="lan"
-        title="Relays"
-        onPress={() => navigation.navigate('Relays')}
-      />
-      <ButtonSettings
-        icon="shield-key-outline"
+        icon="shield-key"
         title="Key"
         onPress={() => navigation.navigate('Key')}
       />
+      <Divider />
       <ButtonSettings
-        icon="information-outline"
+        icon="cellphone-off"
+        title="Logout"
+        onPress={() => ModalController.showModal('Logout', <LogoutModal />)}
+      />
+      <Divider />
+      <Text variant="labelMedium" style={styles.title}>
+        App settings
+      </Text>
+      <ButtonSettings
+        icon="brightness-4"
+        title="Theme"
+        onPress={() => ModalController.showModal('Theme', <SelectThemeModal />)}
+      />
+      <Divider />
+      <ButtonSettings
+        icon="server-network"
+        title="Relays"
+        onPress={() => navigation.navigate('Relays')}
+      />
+      <Divider />
+      <Text variant="labelMedium" style={styles.title}>
+        App information
+      </Text>
+      <ButtonSettings
+        icon="information"
         title="About"
         onPress={() => {
           console.log('pressed');
         }}
       />
-      <ButtonSettings
-        icon="logout"
-        title="Logout"
-        onPress={() => ModalController.showModal('Logout', <LogoutModal />)}
-      />
+      <Divider />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    marginLeft: 20,
+    marginTop: 20,
+    marginBottom: 3,
+  },
+});
 
 export default SettingsScreen;
